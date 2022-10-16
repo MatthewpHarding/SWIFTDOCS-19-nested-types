dd8dfa11e3d708bfa998c820d902776dbe4c15f8
Matthew Harding <matthewpharding@gmail.com>
2022-10-16 11:08:34 +0100

    Added the page number (from official Swift documentation) to each Xcode playground file.

--- Nested Types.playground/Pages/Nested Types.xcplaygroundpage/Contents.swift
+++ Nested Types.playground/Pages/Nested Types.xcplaygroundpage/Contents.swift
@@ -1,6 +1,6 @@
 //: ![Swift](swift-logo.png)
 //: Swift v5.7 [official docs](https://docs.swift.org/swift-book/LanguageGuide/NestedTypes.html)
-//: # Nested Types
+//: # 20. Nested Types
 //:
 //:
 //: Enumerations are often created to support a specific class or structure’s functionality. Similarly, it can be convenient to define utility classes and structures purely for use within the context of a more complex type. To accomplish this, Swift enables you to define nested types, whereby you nest supporting enumerations, classes, and structures within the definition of the type they support.
